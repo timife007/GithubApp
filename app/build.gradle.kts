084dd82ea461b6204253360f1f16b446d2b11b22
timothy ademola <timife007@gmail.com>
2024-07-30 17:30:48 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.compose.compiler)
+
+}
+
+android {
+    namespace = "com.timife.skiywatertest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.timife.skiywatertest"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.room)
+    implementation(libs.roomKtx)
+    implementation(libs.hilt)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.retrofit)
+    implementation(libs.coroutinesAdapter)
+    implementation(libs.coroutineCore)
+    implementation(libs.coroutineAndroid)
+    implementation(libs.viewModelCompose)
+    implementation(libs.moshi)
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.roomCompiler)
+    implementation(libs.timber)
+    implementation(libs.coroutineCore)
+    implementation(libs.coroutineAndroid)
+    implementation(libs.coroutinesAdapter)
+    implementation(libs.hiltCompose)
+    implementation(libs.lifecycle)
+    implementation(libs.materialIcons)
+    implementation(libs.coil)
+    implementation(libs.okHttp)
+    implementation(libs.interceptor)
+//    implementation(libs.hilt.compiler)
+    implementation(libs.chuckerLib)
+    testImplementation(libs.junit)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
